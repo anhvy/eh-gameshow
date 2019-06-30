b90d7521a89c37cdc4c639bf899c2cd932adf183
anhvy <vy.nguyen@employmenthero.com>
2019-06-30 20:00:21 +0700

    Re-enable answer

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -90,13 +90,13 @@ class RoundsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_round
-      @round = Round.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_round
+    @round = Round.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def round_params
-      params.require(:round).permit(:episode_id, :question_ids)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def round_params
+    params.require(:round).permit(:episode_id, :question_ids)
+  end
 end
